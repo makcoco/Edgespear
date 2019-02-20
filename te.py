fcc5edf500b63fbfb760d2b71fbe829b54975ac6
Edge-X <470203197@qq.com>
2019-02-20 18:45:40 +0800

    改

--- te.py
+++ te.py
@@ -1,26 +1,26 @@
 import random
 
 cards = ['1', '1', '1', '1', '2', '2', '2', '2', '3', '3', '3', '3', '4', '4', '4', '4', '5', '5', '5', '5', '6',
-		 '6', '6', '6', '7', '7', '7', '7', '8', '8', '8', '8', '9', '9', '9', '9', '10', '10', '10', '10', '11',
-		 '11', '11', '11', '12', '12', '12', '12', '13', '13', '13', '13']
+        '6', '6', '6', '7', '7', '7', '7', '8', '8', '8', '8', '9', '9', '9', '9', '10', '10', '10', '10', '11',
+         '11', '11', '11', '12', '12', '12', '12', '13', '13', '13', '13']
 class player(object):
-	def __init__(self,card):
-		self.card = card
-	def go_fish():
-		pass
-	def ask():
-		pass
-#LlLlLLALALALALALALAL
+    def __init__(self,card):
+        self.card = card
+    def go_fish():
+        pass
+    def ask():
+        pass
+
 def fapai():
 
-	player = []
-	for i in range(0,5):
-		a = random.randint(0,len(cards))
-		card = cards.pop(a -1 )
-		player.append(card)
-	return player
+    player = []
+    for i in range(0,5):
+        a = random.randint(0,len(cards))
+        card = cards.pop(a -1 )
+        player.append(card)
+    return player
 player1=player(fapai())
 
 print(player1.card,
-	  #"\n",player2.card,'\n',player3.card,'\n',player4.card,'\n',
-		cards)
+      #"\n",player2.card,'\n',player3.card,'\n',player4.card,'\n',
+        cards)
