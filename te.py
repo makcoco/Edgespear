2fa305effbee7eb277550f75fc312f553665b65d
Edge-X <470203197@qq.com>
2019-02-21 10:04:23 +0800

    改

--- te.py
+++ te.py
@@ -4,8 +4,8 @@ cards = ['1', '1', '1', '1', '2', '2', '2', '2', '3', '3', '3', '3', '4', '4', '
         '6', '6', '6', '7', '7', '7', '7', '8', '8', '8', '8', '9', '9', '9', '9', '10', '10', '10', '10', '11',
          '11', '11', '11', '12', '12', '12', '12', '13', '13', '13', '13']
 class player(object):
-    def __init__(self,card):
-        self.card = card
+    def __init__(self):
+        self.card = None
     def go_fish():
         pass
     def ask():
@@ -19,7 +19,8 @@ def fapai():
         card = cards.pop(a -1 )
         player.append(card)
     return player
-player1=player(fapai())
+player1=player()
+player1.card=fapai()
 
 print(player1.card,
       #"\n",player2.card,'\n',player3.card,'\n',player4.card,'\n',
