7dedb56b6745d6bd43711a3ec3b022ae725ef846
cocoMAK <mztjk@163.com>
2019-02-21 22:28:18 +0800

    最新

--- go fish.py
+++ go fish.py
@@ -5,15 +5,20 @@ Created on Wed Feb 20 13:20:42 2019
 @author: cocoMAK
 """
 import random
-from textwrap import dedent
+
 
 cards = ['1', '1', '1','1','2','2','2','2','3','3','3','3','4','4','4','4','5','5','5','5','6',
          '6','6','6','7','7','7','7','8','8','8','8','9','9','9','9','10','10','10','10','11',
          '11','11','11','12','12','12','12','13','13','13','13']
 
+
+
 class player(object):
-    def __init__(self):
+    def __init__(self,i):
         self.card = None
+        self.i = i
+        j = i
+
 
     def ask(self):
         print(" Choose a player you want to ask for card.")
@@ -21,7 +26,7 @@ class player(object):
 
         if action == "player1" or "player2" or "player3" or "player4":
             print(f"You choose {action}.")
-            return player.choose_card(self)
+            return player.choose_card(self,action)
 
         else:
             print("You can't choose someone who didn't exit.")
@@ -29,8 +34,14 @@ class player(object):
 
     def go_fish(self):
         pass
-    def choose_card(self):
-        print(f"You have {player().__init__.card}.")
+    def choose_card(self,a):
+        print(self.i)
+        print(f"You have {who[self.i].card}.Which one do you want from {a}.")
+        action = input("> ")
+        print(who[self.i].card)
+
+    def give_card(self):
+        pass
 
 
 
@@ -44,8 +55,13 @@ def fapai():
         i = i + 1
     return player
 
+player1 = player(1)
+player2 = player(2)
+#player3 = player(3)
+#player4 = player(4)
+who = {1: player1, 2: player2,  # 3:player3,4:player4
+               }
 
-player1 = player()
 player1.card = fapai()
 
 player1.ask()
